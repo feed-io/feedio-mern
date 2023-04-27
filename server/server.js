51c1c655f2407eded9e196c741f641b5c6bae004
Luis <ramirezedluis@gmail.com>
2023-04-27 19:57:53 +0200

    basic files

---
+++ server/server.js
@@ -0,0 +1,24 @@
+// Import required modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define routes
+
+
+// Start server
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
