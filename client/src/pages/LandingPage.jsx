1693920b3ef12c95abd5c5058c0253ded0575f04
Luis <ramirezedluis@gmail.com>
2023-08-30 20:57:38 +0200

    make footer, navbar, roomspage, landingpage mobile responsive

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -33,12 +33,9 @@ import Sentiment from "../assets/sentiment.svg";
 import Testimonial from "../assets/testimonial.svg";
 import Analytics from "../assets/analytics.svg";
 
-import React from "react";
-
 const LandingPage = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
 
   const platforms = [
     {
